597fde47cfadd832ea062d3c116e4483d96dd022
ecrseer <juustino@live.com>
2021-12-28 13:17:01 -0300

    alert,required input

--- contatenos.js
+++ contatenos.js
@@ -1,13 +1,28 @@
-function enviarFormulario(ev) {
-    ev.preventDefault()
-    
-    let formulario = new FormData(ev.target)
-    let nome = formulario.get("Nome")
+let fadeOutTimeOutTimer = setTimeout(() => { }, 2000)
+function mostraAlertDialog(texto) {
 
     let dialog = document.querySelector(".alertadialog")
     dialog.classList.add("fadeIn")
+    dialog.textContent = texto
 
+    clearTimeout(fadeOutTimeOutTimer)
+    fadeOutTimeOutTimer = setTimeout(() =>
+        dialog.classList.remove("fadeIn"),
+        4000)
+}
+function ativaCamposObrigatorios() {
+    let inputs = document.getElementsByTagName("input")
+    for (let i = 0; i < inputs.length; i++) {  
+        inputs[i].setAttribute("required","")           
+    } 
+}
+//setTimeout(4700, ativaCamposObrigatorios())
+function enviarFormulario(ev) {
+    ev.preventDefault()
+    ativaCamposObrigatorios()
+    let formulario = new FormData(ev.target)
+    let nome = formulario.get("Nome")
     let email = formulario.get("Email")
     debugger
-     
+
 }
