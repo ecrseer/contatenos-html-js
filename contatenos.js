5fad7db563a863eee589d77ddb2516f65cb86535
ecrseer <juustino@live.com>
2021-12-27 23:33:30 -0300

    alert

--- contatenos.js
+++ contatenos.js
@@ -1,1 +1,13 @@
-//alert('hi')
+function enviarFormulario(ev) {
+    ev.preventDefault()
+    
+    let formulario = new FormData(ev.target)
+    let nome = formulario.get("Nome")
+
+    let dialog = document.querySelector(".alertadialog")
+    dialog.classList.add("fadeIn")
+
+    let email = formulario.get("Email")
+    debugger
+     
+}
